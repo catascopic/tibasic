c3079c8952211dc15a7d6ad66b8c06ba85613473
Max <maxkoren@gmail.com>
2026-05-20 02:30:30 -0400

    Removed some files

--- operations.py
+++
@@ -1,3 +0,0 @@
-# All operator, function, and command implementations have moved to tokens.py
-# as fields on each Token object (binary_op_fn, unary_op_fn, call_fn,
-# execute_fn, value_fn).  This file is intentionally empty.
